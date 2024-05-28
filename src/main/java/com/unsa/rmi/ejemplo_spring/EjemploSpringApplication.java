ca2d33d6f7fcf689d40d5b1c1edb4ac946b80709
AnthonyMTH <aticonaha@unsa.edu.pe>
2024-05-27 23:11:48 -0500

    Iniciando proyecto

---
+++ src/main/java/com/unsa/rmi/ejemplo_spring/EjemploSpringApplication.java
@@ -0,0 +1,14 @@
+package com.unsa.rmi.ejemplo_spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.rmi.RemoteException;
+
+@SpringBootApplication
+public class EjemploSpringApplication {
+	
+	public static void main(String[] args) throws RemoteException {
+
+		
+	}
+
+}
